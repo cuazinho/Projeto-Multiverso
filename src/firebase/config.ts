cb1099cb9dd65609b0830a869e9cf0be18ec7bde
Cauê Tinta <cauetinta4@gmail.com>
2026-02-19 20:22:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4160275679-51f15",
+  "appId": "1:884298873215:web:3a4e103ca64cf099ae824a",
+  "apiKey": "AIzaSyAIPmu0k72OQvfVQKbQiI2ZjxM9IrOliYc",
+  "authDomain": "studio-4160275679-51f15.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "884298873215"
+};
